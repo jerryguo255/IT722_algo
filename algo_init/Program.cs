35a4ec9027413712436efe204129838cbc4edc0d
Jerry Guo <jerryguo255@outlook.com>
2019-07-11 00:36:13 +1200

    first lab

---
+++ algo_init/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace algo_init
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vertexNum = int.Parse(Console.ReadLine());
+
+            List<Vertex> vertexes = new List<Vertex>();
+            for (int i = 0; i < vertexNum; i++)
+            {
+                vertexes.Add(new Vertex(i));
+            }
+            // 0 1 2
+
+
+            string input = Console.ReadLine();
+            // Regex.Split(s,"")
+            string[] words = input.Split(' ');
+            int vertId = int.Parse(words[0]);//本体
+            int neiId = int.Parse(words[1]); //邻居
+
+
+           // TextReader stdin = Console.In;
+            //Console.SetIn(new StreamReader("text.txt"));
+            while (vertId != -1 && neiId != -1)
+            {
+                vertexes[vertId].Addneighbour(vertexes[neiId]);
+
+                //Vertex vNei = new Vertex(neiId);
+                ////Vertex v0 = new Vertex(0);
+                ////vertexes.Add(v0);
+                //foreach (var item in vertexes) //0 1 ， 1 0
+                //{
+                //    if (item.Id == vertId)  // 0 1 2
+                //    {
+                //        item.Addneighbour(vNei);
+                //    }
+                //}
+                
+
+
+
+               // Vertex vNei = new Vertex(neiId);
+
+
+                input = Console.ReadLine();
+                words = input.Split(' ');
+                vertId = int.Parse(words[0]);
+                neiId = int.Parse(words[1]);
+            }
+
+            foreach (var item in vertexes)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ algo_init/Vertex.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace algo_init
+{
+    class Vertex : IComparable<Vertex>
+    {
+        public int Id { get; set; }
+        public int IncomingEdge { get; set; }
+
+        public List<Vertex> adj = new List<Vertex>();  //相邻元素的ID
+
+        public Vertex(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public void Addneighbour(Vertex nbr) {
+            adj.Add(nbr);
+            adj.Sort();
+        }
+
+        public override string ToString() {    
+            string neig = null;
+            foreach (var item in adj)
+            {
+                neig += " " + item.Id.ToString();
+            }
+            return string.Format("{0}:{1}", Id,neig);
+        }
+
+        public override bool Equals(object obj)
+        {
+           Vertex other= obj as Vertex;
+            return this.Id == other.Id;
+        }
+
+        public int CompareTo(Vertex other)
+        {
+            return Id - other.Id;
+        }
+    }
+}
