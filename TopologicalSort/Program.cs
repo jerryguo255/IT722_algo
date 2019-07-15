fe24c1a3f4d665c9aa00451f9bd5d7d5fd4dd637
Zijie Guo <32231346+jerryguo255@users.noreply.github.com>
2019-07-15 14:19:09 +1200

    finished  v0.1

--- TopologicalSort/Program.cs
+++ TopologicalSort/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,63 @@ namespace TopologicalSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+
+            List<VertexforToplog> sortedList = new List<VertexforToplog>();
+            //1 create a target list
+
+            // StreamReader stdin = new StreamReader("..\\..\\topologicalSort.txt");
+            // Console.SetIn(stdin);//standard input from txt file
+
+
+            //2 init empty vertex list (number of vertex)
+            int vertexNum = int.Parse(Console.ReadLine());// get the number of tasks  :3
+            List<VertexforToplog> startList = new List<VertexforToplog>();
+            for (int i = 0; i < vertexNum; i++)
+            {
+                startList.Add(new VertexforToplog(Console.ReadLine()));
+                //add data  0:go  1:ready  2:set
+            }
+            //2 End
+
+
+            //3 load des pairs
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+            int vertexIndex = int.Parse(words[0]);
+            int neibVertexIndex = int.Parse(words[1]);
+            startList[vertexIndex].Addneighbour(startList[neibVertexIndex]);//first  dst pair
+            startList[neibVertexIndex].IncomingEdge += 1;
+
+            input = Console.ReadLine();
+            words = input.Split(' ');
+            vertexIndex = int.Parse(words[0]);
+            neibVertexIndex = int.Parse(words[1]);
+            startList[vertexIndex].Addneighbour(startList[neibVertexIndex]);//second  dst pair
+            startList[neibVertexIndex].IncomingEdge += 1;  //add 1 to neibour incomeEdge
+            //3 End
+
+            while (startList.Count != 0)
+            {
+                for (int i = 0; i < startList.Count; i++)
+                {
+                    if (startList[i].IncomingEdge == 0)//if the vertex's incomeEdge
+                    {
+                        if (startList[i].adj.Count != 0)  //if the vertex has a neib
+                        {
+                            startList[i].adj[0].IncomingEdge = 0;//set the neib's vertex to 0
+                        }
+                        sortedList.Add(startList[i]);         //add the vertex to sorted list
+                        startList.RemoveAt(i);        //remove the vertex from origin list
+                    }
+                }
+            }
+            // print out the sorted list
+
+            foreach (var item in sortedList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ TopologicalSort/VertexforToplog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TopologicalSort
+{
+    class VertexforToplog //: IComparable<VertexforToplog>
+    {
+        public int Id { get; set; }
+        public string Data { get; set; }
+        public int IncomingEdge { get; set; }
+
+        public List<VertexforToplog> adj = new List<VertexforToplog>();  //相邻元素的ID
+
+        public VertexforToplog(string Data)
+        {
+            this.Data = Data;
+        }
+
+        public void Addneighbour(VertexforToplog nbr)
+        {
+            if (!adj.Contains(nbr))
+            {
+                adj.Add(nbr);
+            }
+            //adj.Sort();
+        }
+
+        public override string ToString()
+        {
+            return Data;
+        }
+
+        //public override bool Equals(object obj)
+        //{
+        //    VertexforToplog other = obj as VertexforToplog;
+        //    return this.Id == other.Id;
+        //}
+
+        //public int CompareTo(VertexforToplog other)
+        //{
+        //    return Id - other.Id;
+        //}
+    }
+}
